e4910d8c95ee320c756fd9dd537010c775b25762
yappy <superunyo@yahoo.co.jp>
2024-06-09 11:38:14 +0900

    inbox - spawn tokio tasks

--- src/commands/inbox.rs
+++ src/commands/inbox.rs
@@ -1,68 +1,80 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::Arc;
 
 use anyhow::{Context, Result};
 use getopts::Options;
+use tokio::runtime::Runtime;
 
 use super::{Config, DIRNAME_INBOX};
 
-#[derive(Default, Clone, Copy)]
+#[derive(Default)]
 struct ProcessStat {
-    processed: u32,
-    error: u32,
+    processed: AtomicU32,
+    error: AtomicU32,
 }
 
-fn process_file(mut stat: ProcessStat, filepath: &Path) -> ProcessStat {
-    println!("Process {}", filepath.to_string_lossy());
-
-    stat.processed += 1;
-    stat
-}
-
-fn process_dir(mut stat: ProcessStat, dirpath: &Path) -> ProcessStat {
+async fn process_dir(stat: Arc<ProcessStat>, dirpath: PathBuf) {
     println!("Process {}", dirpath.to_string_lossy());
 
+    // get directory iterator
     let iter = match dirpath.read_dir() {
         Ok(iter) => iter,
         Err(err) => {
             println!("{err}");
-            stat.error += 1;
-            return stat;
+            stat.error.fetch_add(1, Ordering::Relaxed);
+            return;
         }
     };
 
+    // for each entry
+    let mut handles = Vec::new();
     for entry in iter {
         let entry = match entry {
             Ok(entry) => entry,
             Err(err) => {
                 println!("{err}");
-                stat.error += 1;
+                stat.error.fetch_add(1, Ordering::Relaxed);
                 continue;
             }
         };
+
         let path = entry.path();
-        if path.is_symlink() {
-            println!("Symbolic link is found. Ignored.");
-            stat.error += 1;
-        } else if path.is_dir() {
-            stat = process_dir(stat, &path);
-        } else if path.is_file() {
-            stat = process_file(stat, &path);
+        if path.is_file() {
+            // execute on a separated thread
+            let h = tokio::spawn(process_file(Arc::clone(&stat), path));
+            handles.push(h);
         } else {
-            println!("Unknown file type");
-            stat.error += 1;
+            println!("Not a regular file {}", path.to_string_lossy());
+            stat.error.fetch_add(1, Ordering::Relaxed);
         }
     }
+    for h in handles {
+        h.await.unwrap();
+    }
+}
 
-    stat
+async fn process_file(stat: Arc<ProcessStat>, filepath: PathBuf) {
+    println!("Process start: {}", filepath.to_string_lossy());
+
+    let sec = (stat.processed.load(Ordering::Relaxed) % 5) as u64;
+    tokio::time::sleep(std::time::Duration::from_secs(sec)).await;
+
+    println!("Process end: {}", filepath.to_string_lossy());
+
+    // TODO
+    stat.processed.fetch_add(1, Ordering::Relaxed);
 }
 
 fn process_inbox(dirpath: &Path, mut config: Config) -> Result<Option<Config>> {
     let inbox_path = dirpath.join(DIRNAME_INBOX);
 
-    let stat: ProcessStat = Default::default();
-    let stat = process_dir(stat, &inbox_path);
-    println!("Processed: {}", stat.processed);
-    println!("Error    : {}", stat.error);
+    let stat: Arc<ProcessStat> = Arc::new(Default::default());
+    let rt = Runtime::new()?;
+    rt.block_on(process_dir(Arc::clone(&stat), inbox_path));
+
+    println!("Processed: {}", stat.processed.load(Ordering::Relaxed));
+    println!("Error    : {}", stat.error.load(Ordering::Relaxed));
 
     // update toml
     config.system.update();
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,12 +1,12 @@
+use std::{fs, path::Path, usize};
+
 use anyhow::Result;
 use tempdir::TempDir;
 
 // Return argv[0] (program name)
-// Panic unless argc == 1
 fn get_argv0() -> String {
     let mut args = std::env::args();
     let argv0 = args.next().unwrap();
-    assert_eq!(args.next(), None);
 
     argv0
 }
@@ -24,10 +24,36 @@ fn print_help() -> Result<()> {
 
 #[test]
 fn init() -> Result<()> {
-    let dir = TempDir::new("example")?;
+    let dir = TempDir::new("bkupman-test")?;
     let dirstr = dir.path().to_str().unwrap();
 
     let argv = [&get_argv0(), "-C", dirstr, "init"];
+    bkupman::entry_point(&argv)?;
+
+    Ok(())
+}
+
+fn create_files(dirpath: &Path, count: usize) -> Result<()> {
+    for i in 0..count {
+        let path = dirpath.join(format!("file{i}"));
+        fs::write(&path, "")?;
+        println!("{}", path.to_string_lossy());
+    }
+    Ok(())
+}
+
+#[test]
+fn inbox_many() -> Result<()> {
+    let dir = TempDir::new("bkupman-test")?;
+    let dirpath = dir.path();
+    let dirstr = dirpath.to_str().unwrap();
+
+    let argv = [&get_argv0(), "-C", dirstr, "init"];
+    bkupman::entry_point(&argv)?;
+
+    create_files(&dirpath.join("inbox"), 1000)?;
+
+    let argv = [&get_argv0(), "-C", dirstr, "inbox"];
     bkupman::entry_point(&argv)?;
 
     Ok(())
